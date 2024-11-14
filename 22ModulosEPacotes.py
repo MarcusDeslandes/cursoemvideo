caa3c758f52c06b039d4096dfd334a460759ac2a
Marcus Deslandes <marcus_deslandes@hotmail.com>
2024-11-14 17:29:02 -0300

    Estudos do dia 14/11 sobre Módulos e Pacotes

---
+++ 22ModulosEPacotes.py
@@ -0,0 +1,29 @@
+# MODULAÇÃO
+'''Cria uma pasta no projeto e nela cria uma arquivo .py com os codigos e funções
+que você criou. Para acessa-la basta utilizar o comando import 'nome da pasta'
+para que você a utilize. E então você utiliza o comando igual o de bibilotecas.
+
+Ex: import raontom
+random.randint(n)
+
+VANTAGENS:
+- Organização do código
+- Facilidade de manutenção
+- Ocultação de código detalhado
+- Reutilização em outros projetos (Basta copiar a pasta com os códigos e colar no
+novo projeto.)'''
+
+# PACOTES
+'''É uma pasta que contém módulos onde você pode separar por assuntos
+
+Ex: Separei o pacote em Números, strings, datas, cores... para acessar é só fazer
+TODO O PACOTE: import uteis (uteis é a pasta do novo projeto que criei para estudos)
+APENAS PARTES: from uteis import datas (Acessei apenas os códigos que mexem com datas da pasta uteis)
+
+Para criar um pacote você cria a pasta (ex: uteis) e dentro dessa pasta cria
+outras pastas (ex: cores, datas, numeros, strings)
+
+Dentro de cada pasta de pacote criada tem que ter um arquivo chamado __initt__.py.
+O pycharm cria essa pasta sozinho mas caso não ocorrer isso... CRIE O ARQUIVO
+O pacote só funciona se tiver esse arquivo
+'''

89cda7f1d1e321577de144a1c2ad57247616bdb8
Marcus Deslandes <marcus_deslandes@hotmail.com>
2024-11-14 16:25:20 -0300

    Estudos do dia 14/11 sobre funções

--- Desafio106.py
+++ Desafio106.py
@@ -1,18 +1,36 @@
-def ajuda(com):
-    help(com)
+from time import sleep
+c = ('\033[m',            # 0 - Sem Cores
+     '\033[0;30;41m',     # 1 - Vermelho
+     '\033[0;30;42m',     # 2 - Verde
+     '\033[0;30;43m',     # 3 - Amarelo
+     '\033[0;30;44m',     # 4 - Azul
+     '\033[0;30;45m',     # 5 - Roxo
+     '\033[7;30m'         # 6 - Branco
+     )
 
+def ajuda(com):
+    titulo(f'Acessando o manual do comando \'{com}\'', 4)
+    print(c[6], end='')
+    help(com)
+    print(c[0], end='')
+    sleep(2)
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}  ')
+    print('~' * tam)
+    print(c[0], end='')
+    sleep(1)
+
+#programa principal
 comando = ''
 while True:
+    titulo('SISTEMA DE AJUDA PYHELP', 2)
     comando = str(input('Função ou Biblioteca > '))
-    tamanho = len(comando)
     if comando.upper() == 'FIM':
-        print('~')
-        print('  ATÉ LOGO!  ')
-        print('~')
         break
-
     else:
-        print('~' * tamanho)
-        print(f'  Acessando o manual do comando {comando}  ')
-        print('~' * tamanho)
         ajuda(comando)
+titulo('ATÉ LOGO!', 1)

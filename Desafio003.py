2ea70684a1efe2c7ce256fc1aa3706ef80f3b190
Marcus Deslandes <marcus_deslandes@hotmail.com>
2024-11-13 21:55:59 -0300

    Acrescentei um print vazio como teste

--- Desafio003.py
+++ Desafio003.py
@@ -2,3 +2,4 @@ n1=int(input('Digite um valor: '))
 n2=int(input('Digite outro '))
 s = n1 + n2
 print('A soma entre \033[31m{} \033[me \033[33m{}\033[m vale: \033[32m{}\033[m'.format(n1, n2, s))
+print()

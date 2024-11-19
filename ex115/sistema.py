1486690620894b544ddf0de32102560b83bfd678
Marcus Deslandes <marcus_deslandes@hotmail.com>
2024-11-19 10:42:01 -0300

    Finalização do exercício 115 com um programa de cadastro de pessoas

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -1,12 +1,24 @@
 from ex115.lib.interface import *
+from ex115.lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resposta = menu(['Ver Pessoas Cadastradas', 'Cadastrar Nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        cabecalho('Opcão 1')
+        # Opção de listar o conteúdo de um arquivo
+        lerArquivo(arq)
+
     elif resposta == 2:
-        cabecalho('Opção 2')
+        # Cadastrar uma nova pessoa
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabecalho('Saindo do sistema ... Até logo!')
         sleep(1)
